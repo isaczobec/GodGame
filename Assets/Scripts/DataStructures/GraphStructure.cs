22d305e3ac767b28f08e4b734cf96d1ea29482b5
viktorgb-laptop <viktorgb@kth.se>
2024-07-10 16:25:31 +0200

    Adds structure to GraphStructure.cs and CreateGraphFromMap.cs
    Scripts are not runnable but an initial version of the structure has been detailed in comments

--- Assets/Scripts/DataStructures/GraphStructure.cs
+++ Assets/Scripts/DataStructures/GraphStructure.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class GraphStructure : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Tests on Awake()
+    void Awake()
     {
+
+        //Add/Delete nodes/edges
+        //Print string representation of total graph
+        //Extract certain data (depending on defined order? for example simple numerical order according to added order)
+        //Check certain search algortims (DFS for example)
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private class GraphNode
     {
-        
+        //List of all adjacent nodes
+        //Data content
+        //Identification according to some logic
     }
+
+    public class Graph
+    {
+        //List of all nodes
+        //Ordered Adjacency list
+        //Adjacency matrix (array?)
+    }
+
 }
+
